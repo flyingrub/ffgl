a5eadf4f009101b56e6c210c39938ab2d0285540
flyingrub <flyingrub@protonmail.com>
2019-07-08 15:39:50 +0200

    Update Wobble

--- source/plugins/Wobble/Wobble.cpp
+++ source/plugins/Wobble/Wobble.cpp
@@ -10,8 +10,15 @@ uniform float relativeTime;
 void main()
 {
 	vec2 p = i_uv *2.-1.;
-	float pos = relativeTime + length(p * size);
-    vec2 offset = vec2(cos(pos), sin(pos));
+	vec2 offset = vec2(cos(length(p * size) + relativeTime), sin(length(p * size) + relativeTime));
+	if (mode == 1.)
+    	offset = vec2(cos(length(p * size + relativeTime)), sin(length(p * size + relativeTime)));
+	if (mode == 2.)
+    	offset = vec2(sin(( p.x * p.y ) * size + relativeTime), cos(( p.x * p.y ) * size + relativeTime));
+	if (mode == 3.)
+    	offset = vec2(sin( i_uv.x * size + relativeTime),cos( i_uv.y * size + relativeTime));
+    if (mode == 4.)
+		offset = vec2(sin( p.x * size + relativeTime), cos( p.y * size + relativeTime));
 	fragColor = texture( inputTexture, i_uv+ strength*offset );
 }
 )";
@@ -19,9 +26,12 @@ void main()
 Wobble::Wobble()
 {
 	setFragmentShader( fshader );
-	addParam( speed = ParamRange::create( "speed", 1., { 0.1, 5. } ) );
-	addParam( size = ParamRange::create( "size", 1., { .7, 5. } ) );
-	addParam( speed = ParamRange::create( "strength", 1., { 0.001, .1 } ) );
+	addParam( speed = ParamRange::create( "speed", .5f, { 0.1, 5. } ) );
+	addParam( size = ParamRange::create( "size", 1.f, { .7, 5. } ) );
+	addParam( speed = ParamRange::create( "strength", 0.05f, { 0.001, .1 } ) );
+	addParam( mode = ParamOption::create( "mode", {
+		{ "1" }, { "2" }, { "3" }, { "4" }, { "5" }
+	}));
 }
 
 Wobble::~Wobble()
--- source/plugins/Wobble/Wobble.h
+++ source/plugins/Wobble/Wobble.h
@@ -11,5 +11,6 @@ private:
 	ParamRange::Ptr speed;
 	ParamRange::Ptr size;
 	ParamRange::Ptr strengh;
+	ParamOption::Ptr mode;
 	float relativeTime=0.0f;
 };
